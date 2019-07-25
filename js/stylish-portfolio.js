c68e6ed8a7702bfc51a3f225ac51995bc4e8dd15
dhagandev <dhagan@utexas.edu>
2019-07-25 18:05:54 -0500

    Base portfolio set up

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -9,6 +9,31 @@
     $(this).toggleClass("active");
   });
 
+  $(".return-home").click(function(e) {
+    e.preventDefault();
+    window.location.replace('/');
+  })
+
+  $(".jac-portfolio-item").click(function(e) {
+    e.preventDefault();
+    window.location.replace('jac.html');
+  })
+
+  $(".paws-portfolio-item").click(function(e) {
+    e.preventDefault();
+    window.location.replace('pawsembly.html');
+  })
+
+  $(".rtn-portfolio-item").click(function(e) {
+    e.preventDefault();
+    window.location.replace('redtale.html');
+  })
+
+  $(".rtnapi-portfolio-item").click(function(e) {
+    e.preventDefault();
+    window.location.replace('redtaleapi.html');
+  })
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -42,23 +67,23 @@
 
 })(jQuery); // End of use strict
 
-// Disable Google Maps scrolling
-// See http://stackoverflow.com/a/25904582/1607849
-// Disable scroll zooming and bind back the click event
-var onMapMouseleaveHandler = function(event) {
-  var that = $(this);
-  that.on('click', onMapClickHandler);
-  that.off('mouseleave', onMapMouseleaveHandler);
-  that.find('iframe').css("pointer-events", "none");
-}
-var onMapClickHandler = function(event) {
-  var that = $(this);
-  // Disable the click handler until the user leaves the map area
-  that.off('click', onMapClickHandler);
-  // Enable scrolling zoom
-  that.find('iframe').css("pointer-events", "auto");
-  // Handle the mouse leave event
-  that.on('mouseleave', onMapMouseleaveHandler);
-}
-// Enable map zooming with mouse scroll when the user clicks the map
-$('.map').on('click', onMapClickHandler);
+// // Disable Google Maps scrolling
+// // See http://stackoverflow.com/a/25904582/1607849
+// // Disable scroll zooming and bind back the click event
+// var onMapMouseleaveHandler = function(event) {
+//   var that = $(this);
+//   that.on('click', onMapClickHandler);
+//   that.off('mouseleave', onMapMouseleaveHandler);
+//   that.find('iframe').css("pointer-events", "none");
+// }
+// var onMapClickHandler = function(event) {
+//   var that = $(this);
+//   // Disable the click handler until the user leaves the map area
+//   that.off('click', onMapClickHandler);
+//   // Enable scrolling zoom
+//   that.find('iframe').css("pointer-events", "auto");
+//   // Handle the mouse leave event
+//   that.on('mouseleave', onMapMouseleaveHandler);
+// }
+// // Enable map zooming with mouse scroll when the user clicks the map
+// $('.map').on('click', onMapClickHandler);
